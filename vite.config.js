ef1ce7555568e9f4a65a1193dd48d404c95ad2e2
⫷㊚ç𝖎𝖉×͜×𝖐𝖆𝖌𝖊𝖓𝖔㊚⫸ <sorokomarco@gmail.com>
2024-11-03 06:13:59 +0000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [],
   server: {
     host: '0.0.0.0',
-    hmr: true, // Change this line to false disable auto-refreshing.
-  }
-})
+    hmr: true, // Changez à `false` pour désactiver le rafraîchissement à chaud si nécessaire.
+  },
+  build: {
+    outDir: 'dist', // Dossier de sortie que Vercel utilisera pour servir les fichiers.
+  },
+});
